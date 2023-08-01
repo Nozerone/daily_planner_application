6a74a566f2749f460502ca993f94ad026ca9a2fd
Nozerone <134429390+Nozerone@users.noreply.github.com>
2023-08-01 16:29:00 -0400

    finalized HTML, CSS, JS and README files.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-let curretTime = daysjs();
-
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//function uses Dayjs to set the current date on the header
+  var currentHeaderDate = dayjs().format("dddd, MMMM DD");
+  $("#currentDay").text(currentHeaderDate);
+  var currentHour = dayjs().hour();
+//Function targets elements by id, gets value, parses string argument and returns interger.
+  $(".description").each(function (textarea) {
+    var currentTextAreaHour = parseInt(
+      $(this).parent().attr("id").split("-")[1]
+    );
+    //if statement compares value to current time.
+    if (currentTextAreaHour === currentHour) {
+      $(this).addClass("present");
+    } else if (currentTextAreaHour > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+
+  // Sets event listeners to Save Button class.
+  $(".saveBtn").on("click", function () {
+    //Extracts value from siblings and parent to set key and value to data thats saves to local storage.
+    var textArea = $(this).siblings("textarea").val();
+    var textAreaKey = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(textAreaKey, textArea);
+  });
+  //Retrives data from local storage and displays it text area.
+  $("#hour-9").children("textarea").val(localStorage.getItem("9"));
+  $("#hour-10").children("textarea").val(localStorage.getItem("10"));
+  $("#hour-11").children("textarea").val(localStorage.getItem("11"));
+  $("#hour-12").children("textarea").val(localStorage.getItem("12"));
+  $("#hour-13").children("textarea").val(localStorage.getItem("13"));
+  $("#hour-14").children("textarea").val(localStorage.getItem("14"));
+  $("#hour-15").children("textarea").val(localStorage.getItem("15"));
+  $("#hour-16").children("textarea").val(localStorage.getItem("16"));
+  $("#hour-17").children("textarea").val(localStorage.getItem("17"));
+
 });
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # daily_planner_application
+This is a simple dayly planner application. It displays the current date in its header.
+The body is composed of hour long time slots between the hours of 9am-5pm. Dependeing on the current time, it displays color coded time slots. Slots int he past are displayed in gray, present is displayed in red, and future in green. The user is able to make entries in said time slots that are saves in local storage and persist when the page is reloaded. 
+
+This app does not requeire installation.
+![Alt text](<assets/images/Screenshot 2023-08-01 at 4.10.59 PM.png>)
+![Alt text](<assets/images/Screenshot 2023-08-01 at 4.11.07 PM.png>)
+![Alt text](<assets/images/Screenshot 2023-08-01 at 4.18.32 PM.png>)
+
+
+
+Deployed link:
+
+
